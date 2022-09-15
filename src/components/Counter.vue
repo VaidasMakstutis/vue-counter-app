aee431945117e3f93685ac1c1114541da1673786
Vaidas <vaidas.makstutis@gmail.com>
2022-09-15 20:17:27 +0300

    Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>The count is: {{ count }}</h2>
+    <p>
+      <button class="btn btn-secondary" @click="count += 5">Increment</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  }
+};
+</script>
